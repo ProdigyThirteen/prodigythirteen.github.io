6304c4b847eb34eb13fe4595111e2e1a77f02cd3
ProdigyThirteen <wmonk51@yahoo.co.uk>
2025-06-02 23:46:31 +0100

    Little more work on personal projects page, set up game jams page

--- js/smallerprojects.js
+++ js/smallerprojects.js
@@ -5,16 +5,6 @@ document.querySelectorAll(".see-more").forEach(button => {
   
       document.getElementById("modalTitle").textContent = card.dataset.title;
       document.getElementById("modalText").textContent = card.dataset.text;
-      
-      // Populate tech list (you can extend this logic based on actual data)
-      const features = [...card.querySelectorAll(".features li")].map(li => li.textContent);
-      const techList = document.getElementById("modalTech");
-      techList.innerHTML = "";
-      features.forEach(feature => {
-        const li = document.createElement("li");
-        li.textContent = feature;
-        techList.appendChild(li);
-      });
   
       modal.classList.remove("hidden");
     });
