4d45e30a435c031ef5bc4c5fc321f226898e0c81
ProdigyThirteen <wmonk51@yahoo.co.uk>
2025-06-03 19:32:13 +0100

    Improved nav bar and mobile compatibility

--- js/script.js
+++ js/script.js
@@ -16,4 +16,11 @@ document.addEventListener("DOMContentLoaded", () => {
       link.classList.add("active");
     }
   });
+
+  const toggleBtn = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.mobile-nav');
+
+  toggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
 });
