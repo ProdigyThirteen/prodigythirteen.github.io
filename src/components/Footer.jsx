8cf5bed94af3df4932912a82d0da5b6244c5e622
Will <wmonk51@yahoo.co.uk>
2025-06-08 15:53:00 +0100

    Round two, fight!

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-white dark:bg-neutral-900 text-black dark:text-white p-4 shadow-md p-4 md:px-8 flex justify-between items-center">
-      © {new Date().getFullYear()} William Monk
-    </footer>
-  );
-}
